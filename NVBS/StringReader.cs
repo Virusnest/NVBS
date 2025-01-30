261bc4d2e85a13eda8ba616ba61310cba6eacbaf
Virusnest <kai.maloney.au@gmail.com>
2025-01-31 08:26:16 +1100

    Refactor

--- NVBS/StringReader.cs
+++ NVBS/StringReader.cs
@@ -1,6 +1,66 @@
+using System.Text;
+using System.Text.RegularExpressions;
+using NVBS.Structure;
+
 namespace NVBS;
 
-public class NVBSStringReader {
-  
-  
+public class NVBSStringReader
+{
+  private readonly StringReader _reader;
+  private static Regex BytePattern = new Regex(@"(:?[0-9]*)b");
+  private static Regex ShortPattern = new Regex(@"(:?[0-9]*)s");
+  private static Regex IntPattern = new Regex(@"(:?[0-9]*)");
+  private static Regex LongPattern = new Regex(@"(:?[0-9]*)l");
+  private static Regex FloatPattern = new Regex(@"[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?");
+  private static Regex DoublePattern = new Regex(@"[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?");
+  public NVBSStringReader(StringReader reader)
+  {
+    _reader = reader;
+  }
+
+
+  public NVBSObject Read()
+  {
+    return null;
+  }
+
+  public NVBSMap Destringify()
+  {
+    var map = new NVBSMap();
+    return map;
+  }
+
+  public string ReadString()
+  {
+    if (_reader.Read() == -1)
+    {
+      return string.Empty;
+    }
+    StringBuilder sb = new StringBuilder();
+    if (_reader.Peek() == '"')
+    {
+      _reader.Read();
+      while (_reader.Peek() != '"')
+      {
+        sb.Append((char)_reader.Read());
+      }
+    }
+    return sb.ToString();
+  }
+
+  public NVBSObject ReadNumber() { return null; }
+  public NVBSArray ReadArray() { return null; }
+
+  public NVBSMap ReadMap()
+  {
+    _reader.Read();
+    while (true)
+    {
+      if (_reader.Peek() == '}') break;
+
+
+    }
+    return null;
+  }
+
 }
--- NVBS/StringWriter.cs
+++ NVBS/StringWriter.cs
@@ -1,5 +1,93 @@
+using NVBS.Structure;
+
 namespace NVBS;
 
-public class NVBSStringWriter {
-  
+public class NVBSStringWriter
+{
+  private StringWriter _writer;
+
+  public NVBSStringWriter(StringWriter writer)
+  {
+    _writer = writer;
+  }
+
+  public void Stringify(NVBSMap map)
+  {
+    WriteMap(map);
+  }
+
+  private void WriteMap(NVBSMap map, int indent = 0)
+  {
+    _writer.Write('{');
+    indent += 1;
+    foreach (var item in map)
+    {
+      WriteIndents(indent);
+      _writer.Write($"\"{item.Key}\":");
+      Write(item.Value, indent);
+
+    }
+    WriteIndents(indent);
+    _writer.Write('}');
+  }
+
+  private void WriteIndents(int indent)
+  {
+    _writer.Write('\n');
+    for (int i = 0; i < indent; i++)
+    {
+      _writer.Write(' ');
+    }
+  }
+
+  private void Write(NVBSObject obj, int indent)
+  {
+    switch (obj.Type)
+    {
+      case NVBSTypes.String:
+        WriteString((NVBSString)obj, indent);
+        break;
+      case NVBSTypes.Array:
+        WriteArray((NVBSArray)obj, indent);
+        break;
+      case NVBSTypes.Map:
+        WriteMap((NVBSMap)obj, indent);
+        break;
+      case NVBSTypes.Byte:
+        _writer.Write(((NVBSByte)obj).Data + 'b');
+        break;
+      case NVBSTypes.Short:
+        _writer.Write(((NVBSShort)obj).Data + 's');
+        break;
+      case NVBSTypes.Double:
+        _writer.Write(((NVBSDouble)obj).Data + 'd');
+        break;
+      case NVBSTypes.Float:
+        _writer.Write(((NVBSFloat)obj).Data);
+        break;
+      case NVBSTypes.Long:
+        _writer.Write(((NVBSLong)obj).Data + 'l');
+        break;
+      case NVBSTypes.Int:
+        _writer.Write(((NVBSInt)obj).Data);
+        break;
+    }
+  }
+
+
+  private void WriteArray(NVBSArray obj, int indent)
+  {
+    _writer.Write('[');
+    foreach (var item in obj)
+    {
+      Write(item, indent);
+      _writer.Write(',');
+    }
+    _writer.Write(']');
+  }
+
+  private void WriteString(NVBSString obj, int indent)
+  {
+    _writer.Write($"\"{obj.Data}\"");
+  }
 }
